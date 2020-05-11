c2afeb69528bff53afb28f6e0332091f0ca56175
yy1111.yang <yongezzang@naver.com>
2020-05-11 23:20:07 +0900

    tmp-menu changed

--- src/api/tmp-menu.js
+++ src/api/tmp-menu.js
@@ -1,8 +1,10 @@
 import request from '@/utils/test-request'
+import store from '@/store'
 
 export function getRoutes() {
+  var roleId = store.getters.roles[0]
   return request({
-    url: '/api/v1/SysMenu/tree',
+    url: `/api/v1/SysMenu/tree/${roleId}`,
     method: 'get'
   })
 }
