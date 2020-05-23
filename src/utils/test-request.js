798a8ce84a25d4df8ae8883632513466da0bb594
yy1111.yang <yongezzang@naver.com>
2020-05-23 14:01:33 +0900

    remove unused console

--- src/utils/test-request.js
+++ src/utils/test-request.js
@@ -46,8 +46,6 @@ service.interceptors.response.use(
      */
     response => {
         const res = response
-        console.log('response')
-        console.log(response)
         // if the custom code is not 20000, it is judged as an error.
         if (res.status !== 200) {
             Message({
